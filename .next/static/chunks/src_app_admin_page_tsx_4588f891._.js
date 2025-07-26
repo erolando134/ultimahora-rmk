d076f2bb2b20b4bdcd28f48de8bf4b7379693865
erolando134 <erolando134@gmail.com>
2025-07-26 14:05:26 -0500

    Ignorar carpeta .next y archivos grandes .7z

--- .next/static/chunks/src_app_admin_page_tsx_4588f891._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_51d141e0._.js",
-  "static/chunks/src_7a4f0df7._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_admin_solicitudes_page_tsx_4588f891._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_c30455be._.js",
-  "static/chunks/src_6aa67513._.js"
-],
-    source: "dynamic"
-});
